a7003cac1f65eb95d99efce7d2b565e44c305828
Anuj054 <anujc054@gmail.com>
2025-02-17 22:54:34 +0530

    1

--- app/(root)/(tabs)/chat.tsx
+++ app/(root)/(tabs)/chat.tsx
@@ -1,11 +1,11 @@
 import {  Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Home = () => {
+const Chat = () => {
   return (
     <SafeAreaView className=' flex-1 justify-center items-center'>
-      <Text className="text-blue-600 "> Home</Text>
+      <Text className="text-blue-600 "> Chat</Text>
     </SafeAreaView>
   );
 };
-export default Home;
+export default Chat;
--- app/(root)/(tabs)/rides.tsx
+++ app/(root)/(tabs)/rides.tsx
@@ -1,11 +1,11 @@
 import {  Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Home = () => {
+const Rides = () => {
   return (
     <SafeAreaView className=' flex-1 justify-center items-center'>
-      <Text className="text-blue-600 "> Home</Text>
+      <Text className="text-blue-600 "> Rides</Text>
     </SafeAreaView>
   );
 };
-export default Home;
+export default Rides;
